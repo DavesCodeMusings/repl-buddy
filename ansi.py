a8315fc8aff329aec284c783929581ff3a99dafa
Dave Horton <dhorton668@outlook.com>
2022-11-18 17:16:13 -0600

    major rewrite

--- ansi.py
+++ ansi.py
@@ -9,7 +9,19 @@ class ANSI:
     References:
         https://en.wikipedia.org/wiki/ANSI_escape_code
         https://ispltd.org/mini_howto/ansi_terminal_codes
+        https://www2.ccs.neu.edu/research/gpc/VonaUtils/vona/terminal/vtansi.htm
     """
+
+    # Escape sequences
+    ESC = '\x1B'
+    CSI = ESC + '['     # Control Sequence Introducer
+
+    # Text attibutes
+    NORMAL = 0
+    BOLD = 1
+    DIM = 2
+    UNDERLINE = 4
+    INVERSE = 7
 
     # Basic 3-bit colors
     COLOR_BLACK = 0
@@ -21,14 +33,8 @@ class ANSI:
     COLOR_CYAN = 6
     COLOR_WHITE = 7
 
-    # Text styles
-    A_NORMAL = 0
-    A_BOLD = 1
-    A_DIM = 2
-    A_UNDERLINE = 4
-    A_INVERSE = 7
-
-    # Special keypad keys
+    # Non-alpha-numeric keys
+    KEY_ESC = 0x1B
     KEY_DOWN = 0x102
     KEY_UP = 0x103
     KEY_LEFT = 0x104
@@ -65,83 +71,13 @@ class ANSI:
         '14~': KEY_F4,
     }
 
-    # Escape sequences
-    ESC = '\x1B'
-    RESET = 'c'
-
-    # Control sequences
-    CSI = ESC + '['
-    CUP = 'H'   # move cursor to Line;Col
-    DSR = '6n'  # query cursor position
-    ED2 = '2J'  # erase screen
-    ED3 = '3J'  # erase screen and scrollback
-    EL0 = '0K'  # erase line to end
-    EL1 = '1K'  # erase line to beginning
-    EL2 = '2K'  # erase entire line
-
     def __init__(self):
         self.reset()
         self.cursor = (1,1)
         self.echo = True
         self.use_keypad = True
 
-    def clear(self, clear_scrollback=False):
-        """
-        Remove all text from the display. Does not move cursor.
-        """
-        if (clear_scrollback == True):
-            stdout.write(ANSI.CSI + ANSI.ED3)
-            stdout.write(ANSI.CSI + ANSI.ED2)
-        else:
-            stdout.write(ANSI.CSI + ANSI.ED2)
-
-    def clear_line(self):
-        """
-        Erase the entire line at the current cursor position.
-        """
-        stdout.write(ANSI.CSI + ANSI.EL2)
-
-    def set_color(self, color_pair=(COLOR_WHITE, COLOR_BLACK)):
-        """
-        Set the foreground and background colors. Accepts 0..7 (3-bit) colors as a tuple.
-        Refer to the predefined color constants for possible values.
-        """
-        fg_color, bg_color = color_pair
-        fg_code = fg_color + 30
-        bg_code = bg_color + 40
-        stdout.write(ANSI.CSI + str(fg_code) + ';' + str(bg_code) + 'm')
-
-    color = property(None, set_color)
-
-    def get_cursor(self):
-        """
-        Return cursor (line, col) coordinates as a tuple.
-        """
-        stdout.write(ANSI.CSI + ANSI.DSR)
-        reply = ''  # Expecting reply with no newline in the form: 'ESC[{line};{col}R'
-        while True:
-            ch = stdin.read(1)
-            reply += ch
-            if (ch == 'R'):
-                break
-
-        reply = reply.lstrip('\x1B[')
-        reply = reply.rstrip('R')
-        if (match('[0-9]+;[0-9]+', reply)):
-            line, col = reply.split(';')
-        else:
-            line, col = ('0', '0')  # Coordinates are 1-based, so (0,0) is impossible.
-        
-        return int(line), int(col)
-
-    def set_cursor(self, line_col):
-        """
-        Move cursor to new line, col.
-        """
-        line, col = line_col
-        stdout.write(ANSI.CSI + str(line) + ';' + str(col) + ANSI.CUP)
-
-    cursor = property(get_cursor, set_cursor)
+    ### Entire Screen ###
 
     def reset(self):
         """
@@ -152,10 +88,157 @@ class ANSI:
         self.lines, self.cols = self.cursor  # It will stop at (last line, last col).
         self.cursor = (1,1)
 
-    def set_style(self, style=A_NORMAL):
-        stdout.write(ANSI.CSI + str(style) + 'm')
+    def clear(self,  before_cursor=True, after_cursor=True, clear_scrollback=False):
+        """
+        Erase the screen, either the entire screen (default) or just the
+        lines before/after the cursor line.
+        """
+        if (clear_scrollback == True):
+            stdout.write(ANSI.CSI + '3J')
 
-    style = property(None, set_style)
+        if after_cursor and not before_cursor:
+            control_sequence = 'J'
+        elif before_cursor and not after_cursor:
+            control_sequence = '1J'
+        elif after_cursor and before_cursor:
+            control_sequence = '2J'
+
+        stdout.write(ANSI.CSI + control_sequence)
+
+    ### Scroll Region ###
+
+    def set_scroll_region(self, row_range):
+        """
+        Limit range of rows (inclusive) affected when scrolling up/down.
+        """
+        if len(row_range) != 2:
+            return False
+        top, bottom = row_range
+        stdout.write(ANSI.CSI + '{:d};{:d}r'.format(top, bottom))
+    
+    def clear_scroll_region(self):
+        stdout.write(ANSI.CSI + 'r')
+
+    scroll_region = property(None, set_scroll_region, clear_scroll_region)
+
+    def scroll_up(self):
+        stdout.write(ANSI.CSI + 'S')
+
+    def scroll_down(self):
+        stdout.write(ANSI.CSI + 'T')
+
+    ### Cursor Control ###
+
+    def get_cursor(self):
+        """
+        Return cursor (line, col) coordinates as a tuple.
+        """
+        stdout.write(ANSI.CSI + '6n')
+        reply = ''  # Expecting reply with no newline in the form: 'ESC[{line};{col}R'
+        while True:
+            ch = stdin.read(1)
+            reply += ch
+            if (ch == 'R'):
+                break
+
+        reply = reply.lstrip(ANSI.CSI)
+        reply = reply.rstrip('R')
+        if (match('[0-9]+;[0-9]+', reply)):
+            line, col = reply.split(';')
+        else:
+            line, col = (0, 0)  # Coordinates are 1-based, so (0,0) is impossible.
+
+        return int(line), int(col)
+
+    def set_cursor(self, line_col):
+        """
+        Move cursor to new line, col.
+        """
+        line, col = line_col
+        stdout.write(ANSI.CSI + '{:d};{:d}H'.format(line, col))
+
+    cursor = property(get_cursor, set_cursor)
+
+    def save_cursor(self):
+        """
+        Ask terminal to save the cursor position, text attributes, and colors.
+        """
+        stdout.write(ANSI.ESC + '7')
+
+    def restore_cursor(self):
+        """
+        Restore the previously saved position, attributes, and colors.
+        """
+        stdout.write(ANSI.ESC + '8')
+
+    ### Individual Lines ###
+
+    def clear_line(self, before_cursor=True, after_cursor=True):
+        """
+        Erase the line where the cursor is currently located, either the
+        entire line (default) or before/after the cursor column.
+        """
+        if after_cursor and not before_cursor:
+            control_sequence = 'K'
+        elif before_cursor and not after_cursor:
+            control_sequence = '1K'
+        elif after_cursor and before_cursor:
+            control_sequence = '2K'
+
+        stdout.write(ANSI.CSI + control_sequence)
+
+    ### Individual Characters ###
+
+    def set_attributes(self, attributes_triplet=(NORMAL, COLOR_WHITE, COLOR_BLACK)):
+        """
+        Define text style (bold, inverse, etc.) and colors. Refer to
+        predefined attribute and color constants for possible values.
+        """
+        if len(attributes_triplet != 3):
+            return False
+        style, fg_color, bg_color = attributes_triplet
+        control_sequence = '{:d};'.format(style) if style else ''
+        control_sequence += '{:d};'.format(fg_color+30) if fg_color else ''
+        control_sequence += '{:d};'.format(bg_color+40) if bg_color else ''
+        control_sequence = control_sequence.rstrip(';')
+        control_sequence += 'm'
+        stdout.write(ANSI.CSI + control_sequence)
+
+    def clear_attributes(self):
+        """
+        Reset text attributes and colors back to default.
+        """
+        stdout.write(ANSI.CSI + 'm')
+
+    attributes = property(None, set_attributes, clear_attributes)
+
+    def set_color(self, color_pair=(COLOR_WHITE, COLOR_BLACK)):
+        """
+        Set the foreground and background colors. Refer to predefined
+        color constants for possible values.
+        """
+        if len(color_pair != 2):
+            return False
+        fg_color, bg_color = color_pair
+        stdout.write(ANSI.CSI + '{:d};{:d}m'.format(fg_color+30, bg_color+40))
+
+    def clear_color(self):
+        """
+        Reset colors to default without affecting attributes (bold, etc.)
+        """
+        stdout.write(ANSI.CSI + '39;49m')  # 39;49 represent default.
+
+    color = property(None, set_color, clear_color)
+
+    def set_style(self, style_attr):
+        stdout.write(ANSI.CSI + '{:d}m'.format(style_attr))
+
+    def clear_style(self):
+        stdout.write(ANSI.CSI + ANSI.NORMAL + 'm')
+
+    style = property(None, set_style, clear_style)
+
+    ### Keyboard Input ###
 
     def getch(self):
         """
@@ -171,7 +254,7 @@ class ANSI:
                 if self.echo == True:
                     stdout.write(keypress)
                 ch = ord(keypress)
-                if ch != 0x1B or self.use_keypad == False:  # Not an ESC sequence
+                if ch != ANSI.KEY_ESC or self.use_keypad == False:  # Not an ESC sequence
                     break
                 elif stdin.read(1) == '[':  # ESC[ is Control Sequence Introducer
                     sequence = ''
@@ -183,3 +266,5 @@ class ANSI:
                     ch = ANSI.keypad_sequence.get(sequence)
                     break
         return ch
+
+    ch = property(getch)
